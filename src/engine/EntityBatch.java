110f15bc76f71c5d4ebc79ffa7e68397e60f87ee
parth <parth.27@outlook.com>
2019-05-08 17:48:00 -0500

    Added rendering order system

--- src/engine/EntityBatch.java
+++ src/engine/EntityBatch.java
@@ -1,16 +1,20 @@
 package engine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EntityBatch
 {
     private Map<Entity, ArrayList<Entity>> entityMap;
+//    private Queue<Entity> renderingQueue;
+    private int renderingIndex;
+    private Entity[] renderingOrder;
 
-    public EntityBatch()
+    public EntityBatch(int batchSize)
     {
         entityMap = new HashMap<Entity, ArrayList<Entity>>();
+//        renderingQueue = new LinkedList<Entity>();
+        renderingOrder = new Entity[batchSize];
+        renderingIndex = 0;
     }
 
     public void addEntity(Entity entity)
@@ -21,6 +25,8 @@ public class EntityBatch
             ArrayList<Entity> arrayList = new ArrayList<Entity>();
             arrayList.add(entity);
             entityMap.put(entity, arrayList);
+//            renderingQueue.add(entity);
+            indexEntity(entity);
         }
         else
         {
@@ -34,26 +40,47 @@ public class EntityBatch
         if(!entityMap.containsKey(entities[0]))
         {
             //create new arraylist and add it to the map
-            ArrayList<Entity> arrayList = new ArrayList<Entity>();
-            for(Entity e: entities)
-                arrayList.add(e);
+            ArrayList<Entity> arrayList = new ArrayList<Entity>(Arrays.asList(entities));
             entityMap.put(entities[0], arrayList);
+            indexEntity(entities[0]);
         }
         else
         {
             //Add entity to the pre-existing array list
             for(Entity e: entities)
                 entityMap.get(entities[0]).add(e);
+//            renderingQueue.add(entities[0]);
         }
     }
 
     public void render()
     {
-        if(!entityMap.isEmpty())
+//        if(!entityMap.isEmpty())
+//        {
+//            // Render all entities
+//            for(ArrayList<Entity> arrayList : entityMap.values())
+//                Renderer.drawEntities(arrayList);
+//        }
+//        Queue temp = renderingQueue;
+//        if(!temp.isEmpty())
+//        {
+//            Renderer.drawEntities(entityMap.get(temp.poll()));
+//        }
+
+        for(Entity entity : renderingOrder)
         {
-            // Render all entities
-            for(ArrayList<Entity> arrayList : entityMap.values())
-                Renderer.drawEntities(arrayList);
+            Renderer.drawEntities(entityMap.get(entity));
+        }
+    }
+
+    private void indexEntity(Entity entity)
+    {
+        if(renderingIndex >= renderingOrder.length)
+            System.err.println("Entity Batch size exceeded...");
+        else
+        {
+            renderingOrder[renderingIndex] = entity;
+            renderingIndex++;
         }
     }
 }
