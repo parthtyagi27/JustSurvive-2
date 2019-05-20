e0505e8c0bbc1c5b57dd2a597611e8c6897aa87f
parth <parth.27@outlook.com>
2019-05-19 22:17:48 -0500

    Added health UI elements

--- src/world/Level.java
+++ src/world/Level.java
@@ -9,6 +9,8 @@ import entities.Bullet;
 import entities.Player;
 import entities.Robot;
 import org.lwjgl.glfw.GLFW;
+import ui.HealthBar;
+import ui.HealthIcon;
 
 import java.util.ArrayList;
 
@@ -27,6 +29,8 @@ public class Level
     private static final float maxRobots = 10;
 
     private ArrayList<Robot> robots;
+    private HealthIcon healthIcon;
+    private HealthBar healthBar;
 
     public Level()
     {
@@ -53,14 +57,19 @@ public class Level
         bulletRightThreshold = Player.XPOSITION + Bullet.gunOffset + deltaBulletDistance + Bullet.WIDTH;
 
         robots = new ArrayList<>();
+
+        healthIcon = new HealthIcon(Main.WIDTH - HealthIcon.WIDTH - HealthBar.WIDTH - 10, Main.HEIGHT - HealthIcon.HEIGHT - 5);
+        healthBar = new HealthBar(Main.WIDTH - HealthBar.WIDTH - 5,  Main.HEIGHT - HealthBar.HEIGHT - 10);
+        entityBatch.addEntity(healthIcon);
     }
 
     public void render()
     {
         entityBatch.render();
+        Robot.renderRobots(robots);
         player.render();
         Renderer.drawEntities(bulletList);
-        Robot.renderRobots(robots);
+        healthBar.render();
     }
 
     public void update()
@@ -90,7 +99,7 @@ public class Level
 
         for(int i = 0; i < bulletList.size(); i++)
         {
-            if(bulletList.get(i).positionVector.x() <= -Bullet.WIDTH * 5 || bulletList.get(i).positionVector.x() >= (Main.WIDTH + Bullet.WIDTH) * 5)
+            if(bulletList.get(i).positionVector.x() <= -Bullet.WIDTH || bulletList.get(i).positionVector.x() >= (Main.WIDTH + Bullet.WIDTH))
                 bulletList.remove(i);
             else
                 bulletList.get(i).update();
@@ -122,11 +131,14 @@ public class Level
                     if (robot.isLeft() && bullet.positionVector.x() <= robot.positionVector.x() + Robot.WIDTH / 2)
                     {
                         robot.health -= Bullet.damage;
+                        robot.shader.setUniform("red", 1);
+                        robot.red = true;
                         bulletList.remove(i);
                         bulletList.trimToSize();
                     } else if (!robot.isLeft() && bullet.positionVector.x() + Bullet.WIDTH >= robot.positionVector.x() - Robot.WIDTH / 2)
                     {
                         robot.health -= Bullet.damage;
+                        robot.red = true;
                         bulletList.remove(i);
                         bulletList.trimToSize();
                     }
@@ -136,12 +148,18 @@ public class Level
         catch (Exception ignored)
         {}
 
-
+        healthBar.update(player);
     }
 
     public void perSecondUpdates()
     {
         if(robots.size() <= maxRobots)
             robots.add(new Robot());
+
+        for(Robot robot : robots)
+        {
+            if(robot.red == true)
+                robot.red = false;
+        }
     }
 }
